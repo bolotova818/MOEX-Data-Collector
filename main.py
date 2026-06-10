667de1639d8ef78b2a796e1b95963c627e344fad
bolotova818 <bolotova818@gmail.com>
2026-06-10 16:23:41 +0300

    Add other indexes

--- main.py
+++ main.py
@@ -10,44 +10,54 @@ Base.metadata.create_all(engine)
 SessionLocal = sessionmaker(bind=engine)
 session = SessionLocal()
 
-needed_date = select(func.max(PriceHistory.trade_date))
-max_date = session.scalar(needed_date)
+indexes = [
+    "IMOEX",
+    "RTSI",
+    "MOEXBC",
+]
 
-if max_date == None:
-    used_date = date.today() - timedelta(days=30)
-else:
-    added_time = timedelta(days=1)
-    used_date = max_date+added_time
+for index in indexes:
+    needed_date = select(func.max(PriceHistory.trade_date)).where(PriceHistory.sec_id == index)
+    max_date = session.scalar(needed_date)
 
-params = {"from":used_date,
-          "limit":100,
-          "sort_order":"TRADEDATE",
-          "iss.meta":"off",
-          "iss.json":"extended",
-          "lang":"ru"
-          }
-
-
-response = requests.get(
-    "https://iss.moex.com/iss/history/engines/stock/markets/index/securities/IMOEX.json", params=params
-)
-
-response.raise_for_status()
-
-data = response.json()
+    if max_date == None:
+        used_date = date.today() - timedelta(days=30)
+    else:
+        added_time = timedelta(days=1)
+        used_date = max_date+added_time
 
 
-for record in data[1]["history"]:
-    price = PriceHistory(
-        sec_id=record["SECID"],
-        trade_date=record["TRADEDATE"],
-        open_price=record["OPEN"],
-        high_price=record["HIGH"],
-        low_price=record["LOW"],
-        close_price=record["CLOSE"],
+    params = {
+            "from":used_date,
+            "limit":100,
+            "sort_order":"TRADEDATE",
+            "iss.meta":"off",
+            "iss.json":"extended",
+            "lang":"ru"
+            }
+
+    url = 'https://iss.moex.com/iss/history/engines/stock/markets/index/securities/' + index + '.json'
+    response = requests.get(
+        url=url, params=params
     )
 
-    session.add(price)
+    response.raise_for_status()
 
-session.commit()
+    data = response.json()
+
+
+    for record in data[1]["history"]:
+        price = PriceHistory(
+            sec_id=record["SECID"],
+            trade_date=record["TRADEDATE"],
+            open_price=record["OPEN"],
+            high_price=record["HIGH"],
+            low_price=record["LOW"],
+            close_price=record["CLOSE"],
+        )
+
+        session.add(price)
+
+    session.commit()
 session.close()
+
